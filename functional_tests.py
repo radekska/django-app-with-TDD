50e7c3c12f1ee848c8d712c2741e66220aa45471
rska <bp4fradek@gmail.com>
2021-03-24 22:39:35 +0100

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -10,8 +10,8 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_list_creation_and_retrive(self):	
 		self.browser.get("http://localhost:8000")
-		self.assertIn('To-Do', self.browser.title)
-
+		self.assertIn('To-Do lists', self.browser.title)
+		self.fail("Finish the test later")
 
 if __name__ == "__main__":
 	unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-	def test_home_view(self):
+	def test_home_page_url_mapping(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		html_data = response.content.decode('utf-8')
+		
+		self.assertTrue(html_data.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>', html_data)
+		self.assertTrue(html_data.endswith('</html>'))
+		
 		
