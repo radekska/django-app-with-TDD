a957d9521ba8571d870e49bf5f3bfff7389ea87b
rska <bp4fradek@gmail.com>
2021-03-29 21:23:24 +0200

    Expanding functional_tests.py to test inputbox and to do table

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,7 +13,23 @@ class NewVisitorTest(unittest.TestCase):
 	def test_list_creation_and_retrive(self):	
 		self.browser.get("http://localhost:8000")
 		self.assertIn('To-Do lists', self.browser.title)
-		self.fail("Finish the test later")
+		
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+		
+		inputbox.send_keys('Buy lambo')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy lambo' for row in rows))
+		
+		self.fail('Finish the tests!')
 
 if __name__ == "__main__":
 	unittest.main()
