ef5c22eb0545ddbb8e5b22ad7cab95d31ca0ef56
rska <bp4fradek@gmail.com>
2021-04-05 14:55:07 +0200

    Adding new List model and tests accordingly.

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+class List(models.Model):
+	pass
+
 class Item(models.Model):
 	text = models.TextField(default='')
+	item_list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+	# when deleting List, with models.CASCADE django will delete all items related to that list as well.
+	
+
