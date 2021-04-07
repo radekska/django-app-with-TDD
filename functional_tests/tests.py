15e2c9bf90ea010454427ae995f6eb50d59b038c
rska <bp4fradek@gmail.com>
2021-04-07 21:30:19 +0200

    Adding functional tests for HTML/CSS styling and layouts.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,6 +96,80 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('1: Jump from plane', page_text)
 
 
+	def test_layout_and_style(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512, delta=10
+			)
+
+		inputbox.send_keys('Testing')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.wait_for_row_in_table('1: Testing')
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512, delta=10
+			)
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # No need to use that anymore as from now on will be using Django test runner for FTs.
 # if __name__ == "__main__":
 # 	unittest.main()
