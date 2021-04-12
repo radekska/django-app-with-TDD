6ae5b9079e55ade00796454981e120afa4a01709
rska <bp4fradek@gmail.com>
2021-04-12 21:02:08 +0200

    Fixing static files miss with the deploy.

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,12 +125,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Configure Django App for Heroku.
-# try:
-#     import django_heroku
-#     # Using Django's builtin SQLite3 database for now.
-#     django_heroku.settings(locals(), databases=False, allowed_hosts=False, secret_key=False)
-# except:
-#     pass
+
+if os.environ.get('HEROKU'):
+    import django_heroku
+    # Using Django's builtin SQLite3 database for now.
+    django_heroku.settings(locals(), databases=False, allowed_hosts=False, secret_key=False)
+
 
 # This will automatically configure DATABASE_URL, ALLOWED_HOSTS, WhiteNoise (for static assets), 
 # Logging, and Heroku CI for your application.
