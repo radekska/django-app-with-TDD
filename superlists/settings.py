fda961d066e82bd4055875806011e45f5745e34c
rska <bp4fradek@gmail.com>
2021-04-12 21:48:13 +0200

    Modyfing settings.py.

--- superlists/settings.py
+++ superlists/settings.py
@@ -126,10 +126,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Configure Django App for Heroku.
 
-if os.environ.get('HEROKU'):
-    import django_heroku
-    # Using Django's builtin SQLite3 database for now.
-    django_heroku.settings(locals(), databases=False, allowed_hosts=False, secret_key=False)
+# if os.environ.get('HEROKU'):
+#     import django_heroku
+#     # Using Django's builtin SQLite3 database for now.
+#     django_heroku.settings(locals(), databases=False, allowed_hosts=False, secret_key=False)
 
 
 # This will automatically configure DATABASE_URL, ALLOWED_HOSTS, WhiteNoise (for static assets), 
