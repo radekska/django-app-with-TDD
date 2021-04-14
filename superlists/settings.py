114a3865209a556910336566eb9aa992a55c04aa
rska <bp4fradek@gmail.com>
2021-04-14 19:47:15 +0200

    Should work now.

--- superlists/settings.py
+++ superlists/settings.py
@@ -124,28 +124,28 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-if os.environ.get('HEROKU'):
-    # Boto3
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# if os.environ.get('HEROKU'):
+# Boto3
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-    # AWS
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # A path prefix that will be prepended to all uploads
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# A path prefix that will be prepended to all uploads
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
-    # Django Static Files Directory
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-else:
-     STATIC_URL = '/static/'
-     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Django Static Files Directory
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# else:
+#      STATIC_URL = '/static/'
+#      STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Configure Django App for Heroku.
