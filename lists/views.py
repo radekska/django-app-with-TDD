042728eb068fb2f557d6cfe2853b504b5d55faf6
rska <bp4fradek@gmail.com>
2021-04-17 15:41:24 +0200

    Adding custom form to home page view.

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,10 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 def home_page(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
