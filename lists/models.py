5cf9701002ec095a103b82c758c2d81066dcdb5a
radekska <radoslaw.skalbania@gmail.com>
2021-05-03 22:52:24 +0200

    working on mocks at model layer.

--- lists/models.py
+++ lists/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+
 class List(models.Model):
-	def get_absolute_url(self):
-		return reverse("view_list", args=[self.id])
-	
+    def get_absolute_url(self):
+        return reverse("view_list", args=[self.id])
+
+
 class Item(models.Model):
-	text = models.TextField(default='')
-	# When deleting List, with models.CASCADE django will delete all items related to that list as well.
-	item_list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
-	
-	# Sets of field names that, taken together, must be unique:
-	class Meta:
-		# The id ordering for the object, for use when obtaining lists of objects:
-		ordering = ('id',)
-		# Sets of field names that, taken together, must be unique:
-		unique_together = ('item_list', 'text')
+    text = models.TextField(default='')
+    # When deleting List, with models.CASCADE django will delete all items related to that list as well.
+    item_list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.text
+    # Sets of field names that, taken together, must be unique:
+    class Meta:
+        # The id ordering for the object, for use when obtaining lists of objects:
+        ordering = ('id',)
+        # Sets of field names that, taken together, must be unique:
+        unique_together = ('item_list', 'text')
 
-
+    def __str__(self):
+        return self.text
--- lists/views.py
+++ lists/views.py
@@ -4,43 +4,46 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item, List
 from lists.forms import (
-	EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
-	ItemForm, ExistingListItemForm
+    EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
+    ItemForm, ExistingListItemForm
 )
 
 User = get_user_model()
 
+
 def home_page(request):
-	return render(request, 'home.html', {'form': ItemForm()})
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
-	items_list = List.objects.get(id=list_id)
-	form = ExistingListItemForm(for_list=items_list)
+    items_list = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=items_list)
 
-	if request.method == 'POST':
-		form = ExistingListItemForm(for_list=items_list, data=request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect(items_list)
-			
-	return render(request, 'list.html', {
-		'items_list': items_list,
-		'form': form
-	})
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=items_list, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(items_list)
+
+    return render(request, 'list.html', {
+        'items_list': items_list,
+        'form': form
+    })
+
 
 def new_list(request):
-	form = ItemForm(data=request.POST)
-	if form.is_valid():
-		new_item_list = List.objects.create()
-		form.save(for_list=new_item_list)
-	else:
-		return render(request, 'home.html', {'form': form})
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        new_item_list = List()
+        new_item_list.owner = request.user
+        new_item_list.save()
+        form.save(for_list=new_item_list)
+    else:
+        return render(request, 'home.html', {'form': form})
 
-	return redirect(new_item_list)
+    return redirect(new_item_list)
 
 
 def my_lists(request, email):
-	owner = User.objects.get(email=email)
-	return render(request, 'my_lists.html', {'owner': owner})
-	
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
